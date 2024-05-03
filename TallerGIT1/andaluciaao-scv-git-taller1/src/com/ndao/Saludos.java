a74b67bb7de024b59f025b0c40fc632149fc4e8c
sturfer <167990209+sturfer@users.noreply.github.com>
2024-05-03 12:33:40 +0200

    Cambios realizados
    System.out.println("Sergio");

---
+++ TallerGIT1/andaluciaao-scv-git-taller1/src/com/ndao/Saludos.java
@@ -0,0 +1,22 @@
+package com.ndao;
+
+/**
+ * Formación AndalucíaAO
+ */
+public class Saludos {
+
+	/**
+	 * Método principal
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hola Alejandro");
+		System.out.println("Hola Joaquín");
+		// TODO continúa el código con tu nombre de pila.
+		System.out.println("Hola Joaquín");
+		System.out.println("Hola Sergio");
+	}
+
+}
